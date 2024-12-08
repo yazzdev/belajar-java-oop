54a40218e21662e6059f11e97968bf7a3c8d61b5
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-08 21:09:19 +0700

    Method

--- src/MainPerson.java
+++ src/MainPerson.java
@@ -10,8 +10,13 @@ public class MainPerson {
     System.out.println(person1.address);
     System.out.println(person1.country);
 
+    person1.sayHello("Eko");
+
     Person person2 = new Person();
+
     Person person3;
     person3 = new Person();
+    person3.name = "Sinta";
+    person3.sayHello("Dyaz");
   }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,8 @@ class Person {
   String name;
   String address;
   final String country = "Indonesia";
+
+  void sayHello(String parmName) {
+    System.out.println("Hello " + parmName + " My name is " + name);
+  }
 }
