b8ab367d707fe75a2a12e9cb973feafdfc8c9f89
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-08 23:37:31 +0700

    Import

--- src/belajar/java/oop/application/Application.java
+++ src/belajar/java/oop/application/Application.java
@@ -1,11 +1,13 @@
 package belajar.java.oop.application;
 
-import belajar.java.oop.data.Product;
+import belajar.java.oop.data.*;
 
 public class Application {
   public static void main(String[] args) {
     Product product = new Product("Mac Book Pro", 30_000_000);
     System.out.println(product.name);
     System.out.println(product.price);
+
+    Data data = new Data();
   }
 }
