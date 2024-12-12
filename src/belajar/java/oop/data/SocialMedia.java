04374b1466eb4d65653dedb175d9cef76aaed216
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-13 02:41:46 +0700

    Final Class

---
+++ src/belajar/java/oop/data/SocialMedia.java
@@ -0,0 +1,14 @@
+package belajar.java.oop.data;
+
+class SocialMedia {
+  String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+// Error: Cannot inherit from final class
+//class FakeFacebook extends Facebook {
+//
+//}
