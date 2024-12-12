3b31d1fd4f7f314ddb6264245f5c925697ec805b
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-12 20:42:15 +0700

    equals() Method

--- src/belajar/java/oop/data/Product.java
+++ src/belajar/java/oop/data/Product.java
@@ -1,5 +1,7 @@
 package belajar.java.oop.data;
 
+import java.util.Objects;
+
 public class Product {
   public String name;
   public int price;
@@ -12,4 +14,38 @@ public class Product {
   public String toString() {
     return "Product name : " + name + ", price : " + price;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Product product = (Product) o;
+
+    if (price != product.price) return false;
+    return Objects.equals(name, product.name);
+  }
+
+//  @Override
+//  public boolean equals(Object obj) {
+//    if (obj == this) {
+//      return true;
+//    }
+//
+//    if (!(obj instanceof Product)) {
+//      return false;
+//    }
+//
+//    Product product = (Product) obj;
+//
+//    if (this.price != product.price) {
+//      return false;
+//    }
+//
+//    if (this.name != null) {
+//      return this.name.equals(product.name);
+//    } else {
+//      return product.name == null;
+//    }
+//  }
 }
--- src/belajar/java/oop/data/ProductApp.java
+++ src/belajar/java/oop/data/ProductApp.java
@@ -8,5 +8,8 @@ public class ProductApp {
     System.out.println(product.price);
 
     System.out.println(product);
+
+    Product product2 = new Product("Mac Book Pro", 30_000_000);
+    System.out.println(product.equals(product2));
   }
 }
